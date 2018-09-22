b98f88567ec4129a341b58bf3fdda53616a6ed0d
Kamil <203901@edu.p.lodz.pl>
2018-09-22 22:35:19 +0200

    Changes on first feature branch

--- src/main/java/learning/App.java
+++ src/main/java/learning/App.java
@@ -2,12 +2,10 @@ package learning;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        String info = "Some changes I should add";
+        System.out.println("Hello World!");
     }
 }
