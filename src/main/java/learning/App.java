8dbc6d0a0a46d4bbc1fc2688a438dc0c4f0c9d93
Kamil <203901@edu.p.lodz.pl>
2018-09-22 22:59:32 +0200

    Changed one line

--- src/main/java/learning/App.java
+++ src/main/java/learning/App.java
@@ -5,8 +5,7 @@ package learning;
  */
 public class App {
     public static void main(String[] args) {
-        String info = "Some strange useless changes I should add";
-        String info2 = "Some strange useless changes I should add";
-        String info3 = "Some strange useless changes I should add";
+        String info = "Some changes I should add";
+        String info2 = "Some other changes I should add";
     }
 }
